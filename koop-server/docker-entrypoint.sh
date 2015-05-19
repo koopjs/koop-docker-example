5f5267013556eb8a9a6fb2459601eef356525be5
Kevin Pettijohn <KPettijohn@esri.com>
2015-05-19 09:32:42 -0700

    Fixed the database check

--- koop-server/docker-entrypoint.sh
+++ koop-server/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,15 +6,16 @@ KOOP_DATABASE=${KOOP_DATABASE:-koopdev}
 
 if [ "$1" = 'koop' ]; then
 
-  check_db=`psql -h postgis -U postgres -lqt | grep -Eo "($KOOP_DATABASE)"`
+  check_db="$(psql -h postgis -U postgres -lqt)"
 
-  if [ -z ${check_db+x} ] && [ "$KOOP_DATABASE" = "$check_db" ]; then
+  if grep -q -Eoq "($KOOP_DATABASE)" <<< $check_db; then
     echo "Database $KOOP_DATABASE has already been created... skipping."
   else
     psql -h postgis -U postgres -c "CREATE DATABASE $KOOP_DATABASE TEMPLATE template_postgis;"
   fi
 
   cd /usr/src/koop
+  echo "Starting the koop server..."
   exec node server.js
 fi
 
